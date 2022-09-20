cda977dcf60c990f08d5b6dbcd95eb79705b0313
FuuKowatty <miedzio.2000@gmail.com>
2022-09-20 19:18:07 +0200

    Random array 1/2

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const cardContainer = document.querySelectorAll('.cardSpace');
-
+console.log('length tablicy ' + cardDeck.length)
+let result = drawSeq();
 cardContainer.forEach(e => {
     let img = document.createElement('img');
-    let result = drawSeq();
+
 
 })
 
@@ -10,7 +11,7 @@ cardContainer.forEach(e => {
 function drawSeq() {
     let arr = [];
     for(let i = 0; i<cardContainer.length-1; i++) {
-        let random = Math.floor(Math.random()* cardDeck.length-1);
+        let random = Math.floor(Math.random() * cardDeck.length );
         console.log(random)
 
     }
