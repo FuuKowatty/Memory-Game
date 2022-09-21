7736997ad7dc5e3e3ef61c2df737ae9fd652825b
FuuKowatty <miedzio.2000@gmail.com>
2022-09-21 12:28:31 +0200

    random card set repaired

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 const cardContainer = document.querySelectorAll('.cardSpace');
 let result = drawSeq(); //random set of cards
-// for(let i = 0; i<cardContainer.length; i++) {
-//     let img = document.createElement('img');
-//     img.src = cardDeck[i].face__src;
-//     cardContainer[i].appendChild(img)
-// }
-console.log(result)
+console.log(result.length)
+for(let i = 0; i<cardContainer.length; i++) {
+    let img = document.createElement('img');
+    img.src = cardDeck[result[i]].src;
+    cardContainer[i].appendChild(img);
+    cardContainer[i].addEventListener('click', () => {
+        img.src = cardDeck[result[i]].src;
+    })
+
+}
+
 
 
 
 
 function drawSeq() {
     let arr = [];
-    for(let i = 0; i<cardContainer.length;i++) {
+    for(let i = 0; i<cardContainer.length/2;i++) {
         let random = Math.floor(Math.random() * cardDeck.length );
         if(!arr.includes(random) ) {
             arr.push(random, random)
+        } else {
+            i--;
         }
-        arr.sort(() => .5 - Math.random() );
-    } return arr
+        
+    } 
+    arr.sort(() => .5 - Math.random() );
+    return arr
 }
