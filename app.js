466c453c6d5f78fe3d738cdfab5e15f1dd202af3
FuuKowatty <miedzio.2000@gmail.com>
2022-09-22 15:43:39 +0200

    working beta

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const cardContainer = document.querySelectorAll('.cardSpace');
 let result = drawSeq(); //random set of cards
-console.log(result.length)
-for(let i = 0; i<cardContainer.length; i++) {
+let prevChose = ['nothing'];
+let counter = 0;
+
+
+for(let i = 0; i<cardContainer.length; i++) { 
+
     let img = document.createElement('img');
     img.src = cardDeck[result[i]].face__src;
     cardContainer[i].appendChild(img);
     cardContainer[i].addEventListener('click', () => {
+       
+        counter++
         img.src = cardDeck[result[i]].src;
+        if(prevChose[0] === cardDeck[result[i]].id && prevChose[1] !== cardContainer[i]) {
+                cardContainer[i].innerHTML = '';
+                prevChose[1].innerHTML = '';
+
+        } else if(counter%2 ===0 && prevChose[1] !== cardContainer[i]) {
+                let container = prevChose[1]
+                setTimeout(() => {
+                    img.src = cardDeck[result[i]].face__src; 
+                    container.firstChild.src = './img/faceSide.png';
+                },1000)
+
+
+        }
+
+            prevChose = [cardDeck[result[i]].id, cardContainer[i] ]
     })
 
 }
@@ -19,11 +40,7 @@ function drawSeq() {
     let arr = [];
     for(let i = 0; i<cardContainer.length/2;i++) {
         let random = Math.floor(Math.random() * cardDeck.length );
-        if(!arr.includes(random) ) {
-            arr.push(random, random)
-        } else {
-            i--;
-        }
+        !arr.includes(random) ? arr.push(random, random) : i--;
         
     } 
     arr.sort(() => .5 - Math.random() );
