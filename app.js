a86f5f0c32bfd9fddd2780fba784c0269d172c50
FuuKowatty <miedzio.2000@gmail.com>
2022-09-25 13:07:30 +0200

    Memory Game new code works but sometimes apprs bug

--- app.js
+++ app.js
@@ -1,45 +1,13 @@
 const cardContainer = document.querySelectorAll('.cardSpace');
 let result = drawSeq(); //random set of cards
-let prevChose = [];
 let counter = 0;
-let v;
+let prevChose = [];
 
-for(let i = 0; i<cardContainer.length; i++) { 
-
-
-
-    let img = document.createElement('img');
-    img.src = cardDeck[result[i]].face__src;
-    cardContainer[i].appendChild(img);
-
-    cardContainer[i].addEventListener('click', moveCard)
-       
-        function moveCard() {
-        counter++;           
-
-        img.src = cardDeck[result[i]].src;
-        let prevValueContainer = prevChose[1]
-
-        if(prevChose[0] === cardDeck[result[i]].id && prevValueContainer !== cardContainer[i]) {
-            setTimeout(() => {
-                cardContainer[i].innerHTML = ''; //win
-                prevValueContainer.innerHTML = '';
-            }, 500)
-        } else if(counter%2 ===0 && prevValueContainer !== cardContainer[i]) { //lost
-
-                setTimeout(() => {
-                    img.src = cardDeck[result[i]].face__src; 
-                    prevValueContainer.firstChild.src = './img/faceSide.png';
-                },500)
-        }
-            prevChose = [cardDeck[result[i]].id, cardContainer[i] ];
-
-    }
-    
-
-    
-}
-
+cardContainer.forEach((e,i) => {
+    const {id, name,src,face__src} = cardDeck[ result[i] ];
+    setImages(i, face__src);
+    cardContent(i, src, id, face__src);
+})
 
 
 
@@ -54,3 +22,55 @@ function drawSeq() {
     arr.sort(() => .5 - Math.random() );
     return arr
 }
+
+function setImages(i, face__src) {
+    let img = document.createElement('img');
+    img.src = face__src
+    cardContainer[i].appendChild(img);
+}
+
+
+function cardContent(i, src, id, face__src ) {
+
+    cardContainer[i].addEventListener('click', seeCard);
+
+        function seeCard(e) {
+            let el = e.currentTarget; 
+            let prevChoseContainer = prevChose[1]
+            el.firstChild.src = src;
+
+            if(el !== prevChose[1]) {
+                counter++
+            }
+
+            if(el === prevChose[1]) return
+            if(counter%2===0) { //every second click it will be decided about win 
+                if(id === prevChose[0]) {
+                    winPair(el, prevChoseContainer)
+                } else {
+                    lostPair(el, face__src, prevChoseContainer)
+                }
+            }
+
+            counter%2===1 ? prevChose = [id, el] : prevChose = [];
+            
+        }
+
+
+}
+
+function winPair(e, prevEl) {
+    setTimeout( () => {
+        e.innerHTML = '';
+        prevEl.innerHTML = '';
+    },400)
+}
+
+function lostPair(e, face__src, prevEl) {
+    setTimeout( () => {
+        prevEl.firstChild.src = face__src
+        e.firstChild.src = face__src;
+    },400)
+
+}
+
